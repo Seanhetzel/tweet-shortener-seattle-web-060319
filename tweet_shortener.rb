476f2303f31a7adbbf4810b931df02ea64df65a9
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-19 05:59:26 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -29,7 +29,7 @@ def word_substituter(tweet)
 end
 
 def bulk_tweet_shortener(tweet)
-  shorten_tweet = word_substituter(tweet)
+  #shorten_tweet = word_substituter(tweet)
   tweets.each do |tweet|
     puts shorten_tweet
   end
